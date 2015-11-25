4d2aa561d1589e0af3331652ec3e50f93f8e7d66
dxeware <dxeware@gmail.com>
2015-11-24 17:03:40 -0800

    removed unused var server

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = require('./app/app');
 
 db('mongodb://localhost/college_teams');
 
-var server = app.listen(3000, function() {
+app.listen(3000, function() {
 	console.log('Listening on port 3000.....');
 });
